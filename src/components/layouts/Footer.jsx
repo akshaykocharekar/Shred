deaf2894b58a72c5a1f4ac676a33f27eb9c200ca
akshayKocharekar <akshaykocharekar3@gmail.com>
2025-06-25 21:41:36 +0530

    aurora effect

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Aurora from "../Aurora";
 import telegramIcon from "../../../ShredAssests/icons/telegram.svg";
 import twitterIcon from "../../../ShredAssests/icons/x-social.svg";
 import chainIcon from "../../../ShredAssests/icons/dextools.svg";
@@ -10,20 +10,27 @@ import "../../Styles/Footer.css";
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="socials-wrapper">
-        <p className="socials-text">SOCIALS</p>
-        <div className="icons">
-          <img src={telegramIcon} alt="Telegram" />
-          <img src={twitterIcon} alt="X" />
-          <img src={chainIcon} alt="DexTools" />
-          <img src={owlIcon} alt="Dex Screener" />
+    <>
+      <footer className="footer">
+        <div className="socials-wrapper">
+          <p className="socials-text">SOCIALS</p>
+          <div className="icons">
+            <img src={telegramIcon} alt="Telegram" />
+            <img src={twitterIcon} alt="X" />
+            <img src={chainIcon} alt="DexTools" />
+            <img src={owlIcon} alt="Dex Screener" />
+          </div>
         </div>
-      </div>
-      <div className="paw">
-        <video src={pawVideo} autoPlay loop muted playsInline />
-      </div>
-    </footer>
+        <div className="paw">
+          <video src={pawVideo} autoPlay loop muted playsInline />
+        </div>
+      </footer>
+      <Aurora
+        colorStops={["#660000", "#FF0000", "#660000"]} // Dark crimson → pure red → lava pink
+        amplitude={2.5} // Stronger wave distortion
+        blend={1.7} // Smoother fiery blending
+      />
+    </>
   );
 };
 
